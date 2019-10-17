70e67648b9d9538558534ccca59354b09dc55788
andrewvu-cs <andrewvu.cs@gmail.com>
2019-10-17 11:57:59 -0700

    able to serve html page

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,15 @@
+// Core nodejs module
+const path = require('path');
+
+// Third party node module
 const express = require("express");
 
 const router = express.Router();
 
-// app.use allows us to do a middleware function will be executed after every request
 // admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   console.log("In another middleware!");
-  res.send(
-    '<form action="/admin/add-product" method="POST"> <input type="text" name="title"/><button type="submit">Add product</button></form>'
-  ); // send sets the header for us 'text/html'
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html')) // send sets the header for us 'text/html'
 });
 
 // admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 // get ueses exact match
 router.get("/", (req, res, next) => {
-      console.log("In another middleware!");
-      res.send("<h1>Hello from Express!</h1>"); // send sets the header for us 'text/html'
+      res.sendFile(path.join(__dirname, '../' ,'views', 'shop.html')); // send sets the header for us 'text/html'
     });
 
 module.exports = router;
