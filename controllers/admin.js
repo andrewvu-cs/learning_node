ea051bc8a034c85b8dd360fae19392936a98aaed
andrewvu-cs <andrewvu.cs@gmail.com>
2019-10-18 14:35:10 -0700

    reconfigured product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,24 +1,29 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
-      pageTitle: 'Add Product',
-      path: '/admin/add-product',
-    });
-  };
-  
-  exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
-    product.save();
-    res.redirect('/');
-  };
+  res.render("admin/add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product"
+  });
+};
 
-  exports.getProducts = (req, res, next) => {
-      Product.fetchAll(products => {
-          res.render('admin/products', {
-              prods: products,
-              pageTitle: 'Admin Products',
-              path: '/admin/products'
-          })
-      })
-  }
+exports.postAddProduct = (req, res, next) => {
+//   const title = req.body.title;
+//   const imageUrl = req.body.imageUrl;
+//   const price = req.body.price;
+//   const description = req.body.description;
+  const {title, imageUrl, price, description} = req.body;
+  const product = new Product(title, imageUrl, price, description);
+  product.save();
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products"
+    });
+  });
+};
